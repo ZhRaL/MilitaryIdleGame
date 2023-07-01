e93eff47e5f064741e723446babbf281580aa94f
ZhRaL <71980929+ZhRaL@users.noreply.github.com>
2023-07-01 22:19:44 +0200

    added Chair to git I think

---
+++ Assets/Scripts/Chair.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Chair : MonoBehaviour
+    {
+        private bool _unlocked;
+        public bool Unlocked
+        {
+            get { return _unlocked;}
+            set
+            {
+                gameObject.SetActive(value);
+                _unlocked = value;
+            }
+        }
+
+        public bool Occupied { get; private set; }
+
+        public void ToggleOccupation()
+        {
+            Occupied = !Occupied;
+        }
+    }
+}
