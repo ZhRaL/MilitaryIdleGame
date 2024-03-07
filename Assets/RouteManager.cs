c5cc472a283fa425dc6f4e5f4fe7a9733f132ae7
ZhRaL <71980929+ZhRaL@users.noreply.github.com>
2024-03-07 10:51:04 +0100

    Update RouteManager.cs

--- Assets/RouteManager.cs
+++ Assets/RouteManager.cs
@@ -4,24 +4,28 @@ using UnityEngine;
 
 public class RouteManager : MonoBehaviour
 {
-    private float length;
+    Transform armyRouter, airforceRouter, marineRouter
 
-    public float getRouteLength()
+    public float getRouteLength(DefenseType type)
     {
-        return (length != null) ? length : calculate();
+        switch(type) {
+            case ARMY: return calculate(armyRouter);
+            case AIRFORCE: return calculate(airforceRouter);
+            case MARINE: return calculate(marineRouter);
+        }
     }
 
-    private float calculate()
+    private float calculate(Transform router)
     {
         float sumDistance=0;
 
-        for (int i = 0; i < transform.childCount -1; ++i) {
-            Transform prev = transform.GetChild(i);
-            Transform curr = transform.GetChild(i+1);
+        for (int i = 0; i < router.childCount -1; ++i) {
+            Transform prev = router.GetChild(i);
+            Transform curr = router.GetChild(i+1);
             float distance = Vector3.Distance(prev.position, curr.position);
             sumDistance +=distance;
         }
-        sumDistance += Vector3.Distance(transform.GetChild(0), transform.GetChild(transform.childCount-1));
+        sumDistance += Vector3.Distance(router.GetChild(0), transform.router(transform.childCount-1));
         
         return sumDistance;
     }
