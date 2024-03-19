f92c9944497aede802661d950506e2e685522817
ZhRaL <71980929+ZhRaL@users.noreply.github.com>
2024-03-19 12:31:50 +0100

    Create Enums.cs

---
+++ Assets/Scripts/Util/Enums.cs
@@ -0,0 +1,17 @@
+
+
+namespace DefaultNameSpace {
+
+  public class Enums {
+
+    public enum Rewards {
+      MONEY,BADGES
+    }
+    
+    public enum Costs {
+      MONEY, ADVERTISMENT, GOLD, BADGES
+    }
+    
+    
+  }
+}
