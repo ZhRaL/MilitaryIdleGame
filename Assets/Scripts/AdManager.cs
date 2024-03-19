5e1adb16a0f93464c3456770ec8f371b90a460e2
ZhRaL <71980929+ZhRaL@users.noreply.github.com>
2024-03-19 12:59:42 +0100

    Create AdManager.cs

---
+++ Assets/Scripts/AdManager.cs
@@ -0,0 +1,12 @@
+public class AdManager : MonoBehaviour {
+  public static AdManager INSTANCE;
+
+  private void Awake() {
+    INSTANCE = this;
+  }
+
+  public bool Show() {
+    return false;
+  }
+
+}
