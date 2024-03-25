6e42d87408e4d362d311b7f6767bd8ac9ee29758
ZhRaL <71980929+ZhRaL@users.noreply.github.com>
2024-03-25 09:44:52 +0100

    Create JsonManager.cs

---
+++ Assets/Scripts/JSON/JsonManager.cs
@@ -0,0 +1,12 @@
+
+public class Jsonmanager {
+    public List<JsonController> Controllers = new();
+
+    public void AddManager(JsonController item) {
+      Controllers.Add(item);
+    }
+
+    public string GetSaveString() {
+      // return Jsonhelper.ToString(this);
+    }
+}
