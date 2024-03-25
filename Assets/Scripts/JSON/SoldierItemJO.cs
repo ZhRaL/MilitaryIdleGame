e2db3ab3626b6cbeaacd92be1750ee7e4204628d
ZhRaL <71980929+ZhRaL@users.noreply.github.com>
2024-03-25 09:22:16 +0100

    Create SoldierItemJO.cs

---
+++ Assets/Scripts/JSON/SoldierItemJO.cs
@@ -0,0 +1,8 @@
+
+public class SoldierItemJO : JsonItem {
+    public string Name {get;set;} = "John Doe";
+    public int SpeedLevel {get;set;} = 1;
+    public int MissionRewardLevel {get;set;} = 1;
+    public int CritLevel {get;set;} = 1;
+  
+}
