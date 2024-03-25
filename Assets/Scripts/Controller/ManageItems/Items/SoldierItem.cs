5140f9c3899c9c0e396ee384cb9ce80e164cfdfa
ZhRaL <71980929+ZhRaL@users.noreply.github.com>
2024-03-25 09:28:29 +0100

    Update SoldierItem.cs

--- Assets/Scripts/Controller/ManageItems/Items/SoldierItem.cs
+++ Assets/Scripts/Controller/ManageItems/Items/SoldierItem.cs
@@ -3,6 +3,14 @@
     public class SoldierItem : Item
     {
         public Soldier Soldier { get; set; }
-        
+
+        public override JsonItem ToJson() {
+            return new SoldierJO() {
+                Name = Solder.Name;
+                SpeedLevel = Soldier.LVL_Speed;
+                MissionRewardLevel = Soldier.LVL_Reward;
+                CritLevel =Soldier.LVL_Crit
+            }
+        }
     }
 }
