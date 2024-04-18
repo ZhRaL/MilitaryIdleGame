f9909dedb2efcee002b0c8fda342912fe9722479
Basti <salbert2000@googlemail.com>
2024-04-18 15:17:03 +0200

    DOING NotEnoughmoney

---
+++ Assets/NotEnoughMoney.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class NotEnoughMoney : MonoBehaviour
+{
+    public TMP_Text textAmount;
+
+
+    public void InitMissingMoney(int amount)
+    {
+        // TODO
+    }
+}
